fcd87d8331209cb5d17235706e92a28199b88a01
Alberto Basalo <albertobasalo@hotmail.com>
2024-10-15 14:02:27 +0200

    feat: ng g c

--- AstroBookings/src/app/app.component.ts
+++ AstroBookings/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'lab-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <lab-header></lab-header>
+    <h1>Welcome to {{ title }}!</h1>
+    <p>By {{ author }}</p>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'AstroBookings';
+  title = 'Astro Bookings';
+  author = 'Softtek';
 }
