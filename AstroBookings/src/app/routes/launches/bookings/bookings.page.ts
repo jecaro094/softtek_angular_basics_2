76b246c2b62bc1c6b55670ca73199c68aee3b564
Alberto Basalo <albertobasalo@hotmail.com>
2024-10-29 12:30:36 +0100

    fix: computed current travelers

--- AstroBookings/src/app/routes/launches/bookings/bookings.page.ts
+++ AstroBookings/src/app/routes/launches/bookings/bookings.page.ts
@@ -10,8 +10,10 @@ import {
   WritableSignal,
 } from '@angular/core';
 
+import { BOOKINGS_DB } from '@db/bookings';
 import { LAUNCHES_DB } from '@db/launches';
 import { ROCKETS_DB } from '@db/rockets';
+import { BookingDto } from '@models/booking.dto';
 import { LaunchDto, LaunchStatus, NULL_LAUNCH } from '@models/launch.dto';
 import { NULL_ROCKET, RocketDto } from '@models/rocket.dto';
 import { BookFormComponent } from './book-form.component';
@@ -36,38 +38,44 @@ import { LaunchHeaderComponent } from './launch-header.component';
 })
 export default class BookingsPage {
   // Input signals
+
   /**
    * Launch id, comes from the route :id param
    */
   id: InputSignal<string> = input.required<string>();
 
   // Writable signals
+
   /**
    * New travelers, comes from the book form
    */
   newTravelers: WritableSignal<number> = signal(0);
 
   // Computed signals
+
   /**
    * Launch object, computed from the id
    * Default to NULL_LAUNCH if not found
    */
   launch: Signal<LaunchDto> = computed(
-    () => LAUNCHES_DB.find((l) => l.id === this.id()) || NULL_LAUNCH,
+    () => LAUNCHES_DB.find((launch) => launch.id === this.id()) || NULL_LAUNCH,
   );
   /**
    * Rocket object, computed from the launch
    * Default to NULL_ROCKET if not found
    */
   rocket: Signal<RocketDto> = computed(
-    () => ROCKETS_DB.find((r) => r.id === this.launch().rocketId) || NULL_ROCKET,
+    () => ROCKETS_DB.find((rocket) => rocket.id === this.launch().rocketId) || NULL_ROCKET,
   );
   /**
-   * Current travelers, computed from the rocket capacity and a random number
+   * Current travelers, computed from the number of seats booked for this launch
    */
-  currentTravelers: Signal<number> = computed(() =>
-    Math.floor(this.rocket().capacity * Math.random()),
-  );
+  currentTravelers: Signal<number> = computed(() => {
+    // get the bookings for the launch
+    const bookings = BOOKINGS_DB.filter((booking) => booking.launchId === this.id());
+    // return the number of travelers
+    return bookings.reduce((acc, booking) => acc + booking.numberOfSeats, 0);
+  });
   /**
    * Total travelers, computed from the current travelers and the new travelers
    */
@@ -78,7 +86,7 @@ export default class BookingsPage {
    */
   launchStatus: Signal<LaunchStatus> = computed(() => {
     const occupation = this.totalTravelers() / this.rocket().capacity;
-    if (occupation > 0.9) {
+    if (occupation >= 0.8) {
       return 'confirmed';
     } else {
       return 'delayed';
@@ -86,23 +94,41 @@ export default class BookingsPage {
   });
 
   // Effects
+
   /**
    * Effect to save the launch status to the database
-   * - It is triggered when the launch status changes
-   * - It returns a cloned object to force the template update
+   * - It is triggered when any signal changes
+   * - It changes the database if the launch status changes
+   * - It creates a new booking if a traveler books a seat
+   * - Both are side effects
    */
   saveLaunchEffect = effect(() => {
-    if (this.launch().status === this.launchStatus()) return;
-    const updatedLaunch = {
-      ...this.launch(),
-      status: this.launchStatus(),
+    // signal triggers
+    const newTravelers = this.newTravelers();
+    const launch = this.launch();
+    const status = this.launchStatus();
+    // side effects
+    // update the launch status in the database
+    if (launch.status !== status) {
+      const updatedLaunch = { ...launch, status };
+      const launchIndex = LAUNCHES_DB.findIndex((l) => l.id === launch.id);
+      if (launchIndex === -1) return;
+      LAUNCHES_DB[launchIndex] = updatedLaunch;
+    }
+    // create a new booking in the database
+    const newBooking: BookingDto = {
+      id: `bkg_${BOOKINGS_DB.length + 1}`,
+      travelerId: `usr_t1`,
+      launchId: launch.id,
+      numberOfSeats: newTravelers,
+      totalPrice: launch.pricePerSeat * newTravelers,
+      status: 'pending',
     };
-    const launchIndex = LAUNCHES_DB.findIndex((launch) => launch.id === this.id());
-    if (launchIndex === -1) return;
-    LAUNCHES_DB[launchIndex] = updatedLaunch;
+    BOOKINGS_DB.push(newBooking);
   });
 
   // Methods (event handlers)
+
   /**
    * Event handler for the book form
    * - Triggers when the user books a new traveler
