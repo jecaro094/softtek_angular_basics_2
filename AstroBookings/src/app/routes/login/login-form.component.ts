d273cf0fbee4275e3138d796bcf66784f77f16ca
Alberto Basalo <albertobasalo@hotmail.com>
2024-10-29 14:02:42 +0100

    feat: html relative

--- AstroBookings/src/app/routes/login/login-form.component.ts
+++ AstroBookings/src/app/routes/login/login-form.component.ts
@@ -9,41 +9,7 @@ import { FormsModule } from '@angular/forms';
   selector: 'lab-login-form',
   standalone: true,
   imports: [FormsModule, JsonPipe],
-  template: `
-    <form #loginForm="ngForm">
-      <label for="username">Username</label>
-      <input
-        type="text"
-        id="username"
-        name="username"
-        #usernameInput="ngModel"
-        [(ngModel)]="username"
-        required
-        minlength="3"
-        maxlength="20"
-        [attr.aria-invalid]="usernameInput.invalid" />
-      @if (usernameInput.errors) {
-      <small>{{ usernameInput.errors | json }}</small>
-      }
-      <label for="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        [(ngModel)]="password"
-        #passwordInput="ngModel"
-        required
-        minlength="4"
-        maxlength="20"
-        [attr.aria-invalid]="passwordInput.invalid" />
-      @if (passwordInput.errors) {
-      <small>{{ passwordInput.errors | json }}</small>
-      }
-      <button type="submit" (click)="sendLoginDto.emit()" [disabled]="loginForm.invalid">
-        Login
-      </button>
-    </form>
-  `,
+  templateUrl: './login-form.component.html',
 })
 export class LoginComponent {
   // Model signals (writable input and output)
