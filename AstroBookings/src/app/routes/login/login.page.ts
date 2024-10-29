3347fb846d904ad933aa8e89cc7629c601da7c9a
Alberto Basalo <albertobasalo@hotmail.com>
2024-10-29 13:19:56 +0100

    feat: login page form

--- AstroBookings/src/app/routes/login/login.page.ts
+++ AstroBookings/src/app/routes/login/login.page.ts
@@ -1,19 +1,44 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lab-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   template: `
     <form #f="ngForm">
       <label for="username">Username:</label>
-      <input type="text" id="username" name="username" [(ngModel)]="username" />
+      <input
+        type="text"
+        id="username"
+        name="username"
+        [(ngModel)]="username"
+        #usernameInput="ngModel"
+        required
+        minlength="6"
+        maxlength="10"
+        [attr.aria-invalid]="usernameInput.invalid" />
+      @if(usernameInput.errors) {
+      <small>{{ usernameInput.errors | json }}</small>
+      }
       <label for="password">Password:</label>
-      <input type="password" id="password" name="password" [(ngModel)]="password" />
+      <input
+        type="password"
+        id="password"
+        name="password"
+        [(ngModel)]="password"
+        #passwordInput="ngModel"
+        required
+        minlength="4"
+        maxlength="10"
+        [attr.aria-invalid]="passwordInput.invalid" />
+      @if(passwordInput.errors) {
+      <small>{{ passwordInput.errors | json }}</small>
+      }
       <button type="submit">Login</button>
     </form>
-    <pre>{{ username }}:{{ password }}</pre>
+    <pre>{{ f.value | json }}</pre>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
