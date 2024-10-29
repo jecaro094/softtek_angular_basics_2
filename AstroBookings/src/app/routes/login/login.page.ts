b5b14f6eac0d70ac662fef304c948aa87341b832
Alberto Basalo <albertobasalo@hotmail.com>
2024-10-29 12:58:13 +0100

    feat: ng model

--- AstroBookings/src/app/routes/login/login.page.ts
+++ AstroBookings/src/app/routes/login/login.page.ts
@@ -1,11 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lab-login',
   standalone: true,
-  imports: [],
-  template: ` <p>login works!</p> `,
-  styles: ``,
+  imports: [FormsModule],
+  template: `
+    <form #f="ngForm">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" [(ngModel)]="username" />
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" [(ngModel)]="password" />
+      <button type="submit">Login</button>
+    </form>
+    <pre>{{ username }}:{{ password }}</pre>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class LoginPage {}
+export default class LoginPage {
+  username = 'admin';
+  password = 'secret';
+}
